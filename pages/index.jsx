354c0560a4e0a9af36cccb4e0c178a30e8f155ef
take917 <1tktk2tk3@gmail.com>
2022-04-21 13:04:42 +0900

    🌱 update file

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <p className="text-4xl">welcome to Next.js</p>
+    </Layout>
+  );
+}
