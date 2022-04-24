992576cefdae35479383355a2adeec6620806a0a
take917 <1tktk2tk3@gmail.com>
2022-04-24 11:29:36 +0900

    ✨ add Link compoment

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 export default function Post({ post }) {
   return (
     <div>
       <span>{post.id}</span>
       {":"}
-      <span className="cursor-pointer text-blue border-b border-blue-500 hover:bg-gray-200">
+      <Link href={`/posts/${post.id}`}>
+        {/* <span className="cursor-pointer text-blue border-b border-blue-500 hover:bg-gray-200">
+        </span> */}
         {post.title}
-      </span>
+      </Link>
     </div>
   );
 }
